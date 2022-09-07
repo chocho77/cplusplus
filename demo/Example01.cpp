2a0087af4d1e82c8a9fd780e80c2aebf3cdd663f
chocho77 <chavmom@gmail.com>
2022-09-07 06:22:42 +0300

    add

--- demo/Example01.cpp
+++ demo/Example01.cpp
@@ -5,7 +5,15 @@ class MyClass
 
 public:
     int a = 5;
+
+    int getA();
 };
+
+int MyClass::getA()
+{
+
+    return a;
+}
 
 int main()
 {
@@ -13,6 +21,7 @@ int main()
     MyClass myClass;
 
     std::cout << myClass.a << std::endl;
+    std::cout << myClass.getA() << std::endl;
 
     return 0;
 }
