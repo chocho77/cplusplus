e7dd2fd96bb5b6db11e7331b20475251a079bd47
chocho77 <chavmom@gmail.com>
2022-09-12 05:42:35 -0400

    Changes to be committed: 	new file:   Example02.cpp     Example program for declaration of integer

---
+++ Example02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int number;
+
+    cout << "Enter an integer : ";
+    cin >> number;
+    cout << "Entered integer is : " << endl;
+    cout << "Bye." << endl;
+    
+    return 0;
+}
